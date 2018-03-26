d2b6f8091ee70e07279d4e634e451e90586600b3
Prabodhan Mestry <prabodhan.mestry@tesseractspl.com>
2018-03-26 20:36:53 +0530

    Added comments

--- Harsha/ingesttos3.py
+++ Harsha/ingesttos3.py
@@ -5,16 +5,24 @@ import json
 api_token    = "b09d70ce-d201-4fb9-b3c9-5cd19154f3b7"
 api_base_url = "https://api.appetizeapp.com/v5/report/venues/"
 
+#Create a dictionary to store all the Venue ids
 venuedict = {'Dodgers':'308','Univ':'637','VVCC':'387','BMO':'239','AR':'500'}
+
+#file path to output the details
 filepath = "D:/Projects/Support/Harsha/data.json"
 
 try:
+    #Open a file in write mode
     with open(filepath, 'w') as outfile:
+        #Iterate through each item in dictionary
         for key in venuedict:
+            #call rest api
             response = requests.get(api_base_url + venuedict[key] + "?api_key=" + api_token)
             print(response.json())
             strdata =  json.dumps(response.json())
+            #write the json response to output file
             outfile.write(strdata)
+    #close the file
     outfile.close()  
     print("\nJson file created...")  
     
